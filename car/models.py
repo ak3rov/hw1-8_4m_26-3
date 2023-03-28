c243897a2d1855e0cc72ddc94c0cff4a3156e4da
kyborg <99631503+ak3rov@users.noreply.github.com>
2023-03-28 23:55:46 +0600

    'hw4'

--- car/models.py
+++ car/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class CarShop(models.Model):
     CAR_TYPE = (
         ('Седан', 'Седан'),
@@ -10,16 +9,24 @@ class CarShop(models.Model):
         ("Купе", "Купе"),
         ("Кабриолет", "Кабриолет"),
     )
-    title = models.CharField("Model Name", max_length=100)
-    description = models.TextField("About auto")
+    title = models.CharField("Марка Авто", max_length=100)
+    description = models.TextField("Оо этой машине")
     image = models.ImageField(upload_to='')
     car_type = models.CharField(max_length=100, choices=CAR_TYPE)
-    created_date = models.DateTimeField(auto_now_add=True)
+    created_data = models.DateTimeField(auto_now_add=True)
     cost = models.PositiveIntegerField()
     video = models.URLField()
-    specifications = models.TextField("Characteristic", null=True)
-    model_year = models.TextField("Build year", null=True)
-    manufacturing_country = models.TextField("Country", null=True)
+    reviews = models.TextField('reviews', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class User(models.Model):
+    name = models.CharField("Your name: ", max_length=100)
+    lastname = models.CharField("Your surname: ", max_length=100)
+    email = models.CharField("Your e-mail: ", max_length=100)
+    feedback_text = models.TextField("Your review: ", max_length=200)
 
     def __str__(self):
         return self.title
--- car/views.py
+++ car/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from . import models
+from . import models, forms
+
 
 def car_list_view(request):
     car_object = models.CarShop.objects.all()
@@ -8,3 +10,54 @@ def car_list_view(request):
 def car_detail_view(request, id):
     car_detail = get_object_or_404(models.CarShop, id=id)
     return render(request, 'car_detail.html', {'car_detail': car_detail})
+
+def create_car_view(request):
+    method = request.method
+    if method == "POST":
+        form = forms.CarForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Добавлено в БД")
+    else:
+        form = forms.CarForm()
+    return render(request, "create_car.html", {'form': form})
+
+
+def delete_car_view(request, id):
+    car_object = get_object_or_404(models.CarShop, id=id)
+    car_object.delete()
+    return HttpResponse('Телефон удален из БД')
+
+
+def update_car_view(request, id):
+    car_object = get_object_or_404(models.CarShop, id=id)
+    if request.method == 'POST':
+        form = forms.CarForm(instance=car_object, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Данные обновлены')
+    else:
+        form = forms.CarForm(instance=car_object)
+
+    context = {
+        'form': form,
+        'object': car_object
+    }
+    return render(request, 'update_car.html', context)
+
+
+def reviews_car_view(request, id):
+    car_object = get_object_or_404(models.CarShop, id=id)
+    if request.method == 'POST':
+        form = forms.ReviewsForm(instance=car_object, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Отзыв отправлен')
+    else:
+        form = forms.ReviewsForm(instance=car_object)
+
+    context = {
+        'form': form,
+        'object': car_object
+    }
+    return render(request, 'car_reviews.html', context)
